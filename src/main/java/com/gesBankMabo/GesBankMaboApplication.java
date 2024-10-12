15ce18502ff70c7ab584cb4fd9fdf3b742fcf065
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2024-10-13 00:18:13 +0200

    le commit initial

---
+++ src/main/java/com/gesBankMabo/GesBankMaboApplication.java
@@ -0,0 +1,13 @@
+package com.gesBankMabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesBankMaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesBankMaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gesBankMabo/GesBankMaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesBankMabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesBankMaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
