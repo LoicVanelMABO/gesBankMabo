02651b57005e9c1763dfd67069070c4203f7ba08
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2024-11-02 16:12:36 +0100

    Adding of dto updates

--- src/main/java/com/gesBankMabo/dto/OperationDto.java
+++ src/main/java/com/gesBankMabo/dto/OperationDto.java
@@ -10,13 +10,43 @@ import lombok.NoArgsConstructor;
 public class OperationDto {
     private long compteId;
     private double amount;
+    private String numCompte;
+    private String numCompteSource;
+    private String numCompteDestinataire;
 
     public OperationDto() {
     }
 
-    public OperationDto(long compteId, double amount) {
+    public OperationDto(long compteId, double amount, String numCompte, String numCompteSource, String numCompteDestinataire) {
         this.compteId = compteId;
         this.amount = amount;
+        this.numCompte = numCompte;
+        this.numCompteSource = numCompteSource;
+        this.numCompteDestinataire = numCompteDestinataire;
+    }
+
+    public String getNumCompteSource() {
+        return numCompteSource;
+    }
+
+    public void setNumCompteSource(String numCompteSource) {
+        this.numCompteSource = numCompteSource;
+    }
+
+    public String getNumCompteDestinataire() {
+        return numCompteDestinataire;
+    }
+
+    public void setNumCompteDestinataire(String numCompteDestinataire) {
+        this.numCompteDestinataire = numCompteDestinataire;
+    }
+
+    public String getNumCompte() {
+        return numCompte;
+    }
+
+    public void setNumCompte(String numCompte) {
+        this.numCompte = numCompte;
     }
 
     public long getCompteId() {
